f99c3c5cc3c977a8baf107fae6133240c0fc9ba6
LoneRifle <LoneRifle@users.noreply.github.com>
2024-02-27 11:28:21 +0800

    feat: reset mongodb every 3h

---
+++ bin/reset-db.mongodb.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+db.adminFeedback.deleteMany({ })
+db.adminverifications.deleteMany({ })
+db.payments.deleteMany({ })
+db.myinfohashes.deleteMany({ })
+db.smscounts.deleteMany({ })
+db.tokens.deleteMany({ })
+db.verifications.deleteMany({ })
+db.formIssue.deleteMany({ })
+db.formfeedback.deleteMany({ })
+db.bounces.deleteMany({ })
+db.pendingsubmissions.deleteMany({ })
+db.logins.deleteMany({ })
+db.submissions.deleteMany({ })
+db.forms.deleteMany({ })
+db.workspaces.deleteMany({ })
+db.featureflags.deleteMany({ })
+
+// Leave agencies, users and sessions alone, to ensure users can remain logged in
