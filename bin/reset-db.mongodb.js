ca4ba92d3826bd4984c9f261eaa068852e93700e
littlemight <mfang555@gmail.com>
2025-10-01 12:57:32 +0800

    fix: dont delete featureflags

--- bin/reset-db.mongodb.js
+++ bin/reset-db.mongodb.js
@@ -1,20 +1,20 @@
 /* eslint-disable */
 
-db.adminFeedback.deleteMany({ })
-db.adminverifications.deleteMany({ })
-db.payments.deleteMany({ })
-db.myinfohashes.deleteMany({ })
-db.smscounts.deleteMany({ })
-db.tokens.deleteMany({ })
-db.verifications.deleteMany({ })
-db.formIssue.deleteMany({ })
-db.formfeedback.deleteMany({ })
-db.bounces.deleteMany({ })
-db.pendingsubmissions.deleteMany({ })
-db.logins.deleteMany({ })
-db.submissions.deleteMany({ })
-db.forms.deleteMany({ })
-db.workspaces.deleteMany({ })
-db.featureflags.deleteMany({ })
+db.adminFeedback.deleteMany({});
+db.adminverifications.deleteMany({});
+db.payments.deleteMany({});
+db.myinfohashes.deleteMany({});
+db.smscounts.deleteMany({});
+db.tokens.deleteMany({});
+db.verifications.deleteMany({});
+db.formIssue.deleteMany({});
+db.formfeedback.deleteMany({});
+db.bounces.deleteMany({});
+db.pendingsubmissions.deleteMany({});
+db.logins.deleteMany({});
+db.submissions.deleteMany({});
+db.forms.deleteMany({});
+db.workspaces.deleteMany({});
 
 // Leave agencies, users and sessions alone, to ensure users can remain logged in
+// Leave featureflags alone, to ensure feature flags remain consistent
