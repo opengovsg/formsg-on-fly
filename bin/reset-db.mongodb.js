c946b2f0dacdd2caf4f2bf6dfecef13e1209d795
littlemight <mfang555@gmail.com>
2025-10-08 18:50:42 +0800

    feat: preserve forms for specific users during database reset

--- bin/reset-db.mongodb.js
+++ bin/reset-db.mongodb.js
@@ -12,8 +12,37 @@ db.formfeedback.deleteMany({});
 db.bounces.deleteMany({});
 db.pendingsubmissions.deleteMany({});
 db.logins.deleteMany({});
-db.submissions.deleteMany({});
-db.forms.deleteMany({});
+
+// Get IDs of specific users to preserve their forms
+const openGovUserIds = db.users
+  .find(
+    {
+      email: {
+        $in: [
+          "cheryl@open.gov.sg",
+          "michel@open.gov.sg",
+          "international@open.gov.sg",
+          "demos@open.gov.sg",
+        ],
+      },
+    },
+    { _id: 1 }
+  )
+  .map((u) => u._id);
+
+// Delete forms and submissions NOT created by the users above
+db.submissions.deleteMany({
+  form: {
+    $nin: db.forms
+      .find({ admin: { $in: openGovUserIds } }, { _id: 1 })
+      .map((f) => f._id),
+  },
+});
+
+db.forms.deleteMany({
+  admin: { $nin: openGovUserIds },
+});
+
 db.workspaces.deleteMany({});
 
 // Leave agencies, users and sessions alone, to ensure users can remain logged in
