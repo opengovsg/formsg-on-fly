6257f746920e7e3d56926f2f573a6cbc5df1c0f8
littlemight <mfang555@gmail.com>
2025-10-29 16:44:19 +0800

    fix: exempt ogp emails from user reset

--- bin/reset-users.mongodb.js
+++ bin/reset-users.mongodb.js
@@ -1,13 +1,21 @@
 /* eslint-disable */
 
 printjson(
-  db.users
-  .find(
-    {}, 
+  db.users.find(
+    {},
     { _id: 0, created: 1, lastAccessed: 1, updatedAt: 1 },
-    { sort: ['lastAccessed'] }
+    { sort: ["lastAccessed"] }
   )
-)
+);
 
-db.sessions.deleteMany({ })
-db.users.deleteMany({ })
+db.sessions.deleteMany({});
+db.users.deleteMany({
+  email: {
+    $nin: [
+      "cheryl@open.gov.sg",
+      "michel@open.gov.sg",
+      "international@open.gov.sg",
+      "demos@open.gov.sg",
+    ],
+  },
+});
